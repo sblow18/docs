b01991408c593e4d8f5c9942f138719ccbbc172f
sblow18 <124574880+sblow18@users.noreply.github.com>
2023-02-24 09:17:44 +0000

    Update create-user-and-project.py

--- examples/create-user-and-project.py
+++ examples/create-user-and-project.py
@@ -1,38 +1,39 @@
 import http.client
-import requests
+from pip._vendor import requests
 import json
 import random
 import string
 
 
 # Application Name and Secret must be created in the Run:AI User Interface. See https://docs.run.ai/developer/rest-auth/
-APPNAME = '<key-name>'
-APPSECRET = '<secret>'
-
-
+APPNAME = '<APP NAME>'
+APPSECRET = '<APP SECRET>'
 # the URL is the URL to the Run:AI User Interface
-APPURL = 'https://app.run.ai'
-COMPANYURL = 'https://<company-name>.run.ai'
-
+APPURL = 'https://<RUNAI URL>'
 # REALM is obtained by going to the Run:AI User Interface, and getting the REALM and "Researcher Authentication"
-REALM = '<realm>'
-
+REALM = '<REALM>'
 # The department feature is advanced and is mostly disabled. 
-# Find out the default department ID by listing departments via https://app.run.ai/api/docs/#/Departments/get_v1_k8s_departments
-DEPARTMENT_ID = 1234  
-
+# Find out the default department ID by listing departments via https://app.run.ai/api/docs/#/Departments/get_v1_k8s_departments (Usually is 1)
+DEPARTMENT_ID = <DEPARTMENT_ID>  
 # A tenant (customer) may define multiple clusters. 
 # Find out the Cluster UUID by logging into the Administraor user interface and go to "Clusters"
-CLUSTER_UUID = '000000-0000-0000-0000-0000000000'
+CLUSTER_UUID = '<CLUSTER_UUID>'
+# Base name and email domain to be combined for usernames
+USERDEFAULT = "john"
+EMAILDOMAIN = "@acme.com"
+# Base project name to be used
+PROJECTDEFAULT = "john-project"
+# GPU Allowance for each project
+GPUDEFAULT = <GPU ALLOWANCE>
+# No. of users and projects to create
+QUANTITY = <QUANTITY>
 
 
-#Generate a random string of fixed length 
-def randomString(stringLength=10):
+#Generate a random password of fixed length 
+def randomString(stringLength):
     letters = string.ascii_letters
     digits = string.digits
-    return ''.join(random.choice(letters) for i in range(stringLength)) + random.choice(digits)
-
-
+    return ''.join(random.choice(letters) for i in range(stringLength)) + random.choice(digits) + random.choice(string.punctuation)
 
 def login():
     payload = "grant_type=client_credentials&client_id=" + APPNAME + "&client_secret=" + APPSECRET
@@ -42,7 +43,6 @@ def login():
     r = requests.post(url, headers=headers, data=payload)
 
     if r.status_code //100 == 2:
- #       print (r.text)
         jcontents = json.loads(r.text)
         return jcontents['access_token']
     
@@ -50,31 +50,28 @@ def login():
         print("login error: " + r.text)
         exit(1)
 
-
-
-
 def create_researcher_user(uid, pwd, token):
 
-    payload =  {
-        "roles": ["admin","editor"],
-        "permittedClusters" : [],
+    payload = {
+        "roles":["researcher"],
+        "permittedClusters":[],
         "name":"",
-        "password": pwd,
-        "email" : uid,
+        "password":pwd,
+        "email":uid,
         "permitAllClusters":True,
-        "username" : uid,
-        "needToChangePassword" : True
+        "username":uid,
+        "needToChangePassword":True
     }
-
+    
+    jsonpayload = json.dumps(payload, separators=(',', ':'))
 
     headers = \
-        {'content-type': 'application/json', 
+        {'content-type': 'application/json',
         'Accept': 'application/json',
         'Authorization' : 'Bearer {}'.format(token)}
-    r = requests.post(COMPANYURL + '/v1/k8s/users', json=payload, headers=headers)
+    r = requests.post(APPURL + '/v1/k8s/users', headers=headers, data=jsonpayload)
  
     if r.status_code //100 == 2:
- #       print (r.text)
         jcontents = json.loads(r.text)
         return jcontents['userId']
     
@@ -82,28 +79,26 @@ def create_researcher_user(uid, pwd, token):
         print("login error: " + r.text)
         exit(1)
 
-
-
-
 def create_project(projectname, internalUID, gpuquota, token):
     payload = {
-        "name": projectname,
-        "departmentId": DEPARTMENT_ID,
-        "deservedGpus": gpuquota,
-        "clusterUuid": CLUSTER_UUID,
-        "permissions": {
-            "users": [internalUID]
-        }
+        "name":projectname,
+        "departmentId":DEPARTMENT_ID,
+        "deservedGpus":gpuquota,
+        "clusterUuid":CLUSTER_UUID,
+        "nodeAffinity":{"train":{"affinityType":"no_limit"},"interactive":{"affinityType":"no_limit"}},
+        "permissions":{"users":[internalUID]}
     }
+    
+    jsonpayload = json.dumps(payload, separators=(',', ':'))
+    
     headers = \
         {'content-type': 'application/json', 
         'Accept': 'application/json',
         'Authorization' : 'Bearer {}'.format(token)
         }
-    r = requests.post(COMPANYURL + '/v1/k8s/clusters/' + CLUSTER_UUID + '/projects', json=payload, headers=headers)
+    r = requests.post(APPURL + '/v1/k8s/clusters/' + CLUSTER_UUID + '/projects', headers=headers, data=jsonpayload)
  
     if r.status_code //100 == 2:
-#        print (r.text)
         jcontents = json.loads(r.text)
         return jcontents['id']
     
@@ -111,15 +106,17 @@ def create_project(projectname, internalUID, gpuquota, token):
         print("create project error: " + r.text)
         exit(1)
 
-
-
-def user_setup(username, projectname, gpuquota):
-    access_token = login()
-    pwd = randomString(20)
-    internalUID = create_researcher_user(username, pwd, access_token)
-    print("password: " + pwd)
-    create_project(projectname, internalUID, gpuquota, access_token)
+def user_setup(username, emaildomain, projectname, gpuquota):
+    access_token = login()    
+    for i in range(QUANTITY):
+        pwd = randomString(10)
+        usr = username + str(i) + emaildomain
+        prjtname = projectname + str(i)
+        internalUID = create_researcher_user(usr, pwd, access_token)
+        print("Username: " + usr + " Password: " + pwd)
+        create_project(prjtname, internalUID, gpuquota, access_token)
+        print("Projectname: " + prjtname + " GPU Quota: " + str(gpuquota))   
     return
 
-# Create a user John, with its own Run:AI project and a quota of 2 GPUs.
-user_setup("john@acme.com", "john-project", 2)
+# Create users, with their own Run:AI project and a quota of GPUs.
+user_setup(USERDEFAULT, EMAILDOMAIN, PROJECTDEFAULT, GPUDEFAULT)
